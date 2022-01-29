ce3821c086fd011bb640a51ebe5e484882558ca2
Rumen Guin <rumen2k@gmail.com>
2022-01-29 10:43:42 +0530

    Initial Commit

---
+++ IconGenerator/IconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconGeneratorApp.swift
+//  IconGenerator
+//
+//  Created by RUMEN GUIN on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct IconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
